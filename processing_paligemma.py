bc7461cd09d00f24760159cda025983600f3375b
Om-Alve <omalve8174@gmail.com>
2024-08-19 22:41:27 +0530

    initial commit

---
+++ processing_paligemma.py
@@ -0,0 +1,68 @@
+from typing import Tuple, Dict, Optional, Union, Tuple, Iterable
+import numpy as numpy
+from PIL import Image
+import torch
+
+IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
+IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
+
+
+class PaliGemmaProcessor:
+
+    IMAGE_TOKEN = "<image>"
+
+    def __init__(self, tokenizer, num_image_tokens: int, image_size: int):
+        super().__init__()
+
+        self.image_seq_length = num_image_tokens
+        self.image_size = image_size
+
+        tokens_to_add = {"additional_special_tokens": [self.IMAGE_TOKEN]}
+        tokenizer.add_special_tokens(tokens_to_add)
+        EXTRA_TOKENS = [f"<loc{i:04d}>" for i in range(1024)]
+        EXTRA_TOKENS += [f"<seg{i:03d}>" for i in range(128)]
+        tokenizer.add_tokens(EXTRA_TOKENS)
+        self.image_token_id = tokenizer.convert_tokens_to_ids(self.IMAGE_TOKEN)
+        tokenizer.add_bos_token = False
+        tokenizer.add_eos_token = False
+
+        self.tokenizer = tokenizer
+
+    def __call__(
+        self,
+        text: List[str],
+        images: List[Image.Image],
+        padding: str = "longest",
+        truncation: bool = True,
+    ) -> dict:
+
+        pixel_values = process_images(
+            images,
+            size=(self.image_size, self.image_size),
+            resample=Image.Resampling.BICUBIC,
+            rescale_factor=1 / 255.0,
+            image_mean=IMAGENET_STANDARD_MEAN,
+            image_std=IMAGENET_STANDARD_STD,
+        )
+
+        pixel_values = np.stack(pixel_values, axis=0)
+        pixel_values = torch.tensor(pixel_values)
+
+        input_strings = [
+            add_image_tokens_to_prompt(
+                prefix_prompt=prompt,
+                bos_token=self.tokenizer.bos_token,
+                image_seq_length=self.image_seq_length,
+                image_token=self.IMAGE_TOKEN,
+            )
+            for prompt in text
+        ]
+
+        inputs = self.tokenizer(
+            input_strings,
+            return_tensors="pt",
+            padding=padding,
+            truncation=truncation,
+        )
+
+        return inputs

28c194a1d85c79d1bdb310f40c61fc89bd560244
Om-Alve <omalve8174@gmail.com>
2024-08-19 23:07:52 +0530

    image preprocessing

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -1,10 +1,70 @@
-from typing import Tuple, Dict, Optional, Union, Tuple, Iterable
-import numpy as numpy
+from typing import Tuple, Dict, List, Optional, Union, Tuple, Iterable
+import numpy as np
 from PIL import Image
 import torch
 
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
+
+
+def resize(
+    image: Image.Image,
+    size=Tuple[int, int],
+    resample: Image.Resampling = None,
+    reducing_gap: Optional[int] = None,
+) -> Image.Image:
+    height, width = size
+
+    resized_image = image.resize(
+        (height, width), resample=resample, reducing_gap=reducing_gap
+    )
+
+    return resized_image
+
+
+def rescale(
+    image: np.ndarray, scale: float, dtype: np.dtype = np.float32
+) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
+
+
+def normalize(
+    image: np.ndarray,
+    mean: Union[float, Iterable[float]],
+    std: Union[float, Iterable[float]],
+) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image
+
+
+def process_images(
+    images: List[Image.Image],
+    size: Dict[str, int] = None,
+    resample: Image.Resampling = None,
+    rescale_factor: float = None,
+    image_mean: Optional[Union[float, List[float]]] = None,
+    image_std: Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+
+    height, width = size[0], size[1]
+
+    images = [
+        resize(image=image, size=[height, width], resample=resample) for image in images
+    ]
+
+    images = [np.array(image) for image in images]
+
+    images = [rescale(image, scale=rescale_factor) for image in images]
+
+    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+
+    images = [image.transpose(2, 0, 1) for image in images]
+
+    return images
 
 
 class PaliGemmaProcessor:
